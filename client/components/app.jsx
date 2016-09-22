b955b146f530e268e80ee0274a8cfbb6c9a47dbd
Jason Holtkamp <holtkam2@gmail.com>
2016-09-21 22:06:53 -0500

    added a front end skeleton

---
+++ client/components/app.jsx
@@ -0,0 +1,28 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+
+  }
+
+  componentDidMount(){
+
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+
+};
+
+window.App = App;
+
+
+
