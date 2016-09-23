dc95b9d62863936dbb133018edd15b1d02e8491a
Patrik Korzinski <patrik.korzinski@gmail.com>
2016-09-23 08:54:25 -0700

    Updated package.json, serverSpec tests.

--- spec/serverSpec.js
+++ spec/serverSpec.js
@@ -1,11 +1,33 @@
 var path = require('path');
+
+var assert = require('chai').assert;
 var expect = require('chai').expect;
+var should = require('chai').should();
 
 var server = require(path.join(__dirname, '..', './server/server.js'));
 var routes = require(path.join(__dirname, '..', './server/routes.js'));
 var config = require(path.join(__dirname, '..', './server/config.js'));
 
-describe('Server', function () {
+xdescribe('Server', function () {
+  describe('server.js', function() {
+    'use strict';
+
+    it('exists', function () {
+      expect(SOMETHING_HERE).to.be.a('function');
+
+    });
+
+    it('does something', function () {
+      expect(true).to.equal(false);
+    });
+
+    it('does something else', function () {
+      expect(true).to.equal(false);
+    });
+  })
+});
+
+xdescribe('Routes', function () {
   'use strict';
 
   it('exists', function () {
@@ -22,24 +44,7 @@ describe('Server', function () {
   });
 });
 
-describe('Routes', function () {
-  'use strict';
-
-  it('exists', function () {
-    expect(SOMETHING_HERE).to.be.a('function');
-
-  });
-
-  it('does something', function () {
-    expect(true).to.equal(false);
-  });
-
-  it('does something else', function () {
-    expect(true).to.equal(false);
-  });
-});
-
-describe('Config', function () {
+xdescribe('Config', function () {
   'use strict';
 
   it('exists', function () {
