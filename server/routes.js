a611b6c1b1bf88e70e8cd650f9e34cfb4a913702
Patrik Korzinski <pkorzinski@users.noreply.github.com>
2016-09-27 15:52:18 -0700

    (fix) fixed style (#72)
    * (feat) Expanded backend routing, added database tests.
    
    * Update package.json
    
    * (fix) Fixed test in package.json
    
    * (fix) fixed style

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router(); // eslint-disable-line new-cap
-//const path = require('path');
+// const path = require('path');
 const helper = require('./helpers');
 // Stormpath for auth
 const stormpath = require('express-stormpath');
@@ -18,15 +18,13 @@ router.get('/api/users', helper.getAllUsers);
 router.post('/api/users', helper.createUser);
 router.get('/api/users/:username', helper.getUser);
 router.delete('/api/users/:username', helper.deleteUser);
-//router.put('/api/users/:username', helper.updateUser);
+// router.put('/api/users/:username', helper.updateUser);
 
 // button routes
 // router.get('/api/buttons', helper.getAllButtons);
 // router.post('/api/buttons', helper.createButton);
 // router.get('/api/buttons/:buttonname', helper.getButton);
 // router.delete('/api/buttons/:buttonname', helper.deleteButton);
-//router.put('/api/buttons/:buttonname', helper.updateButton);
-
-
+// router.put('/api/buttons/:buttonname', helper.updateButton);
 
 module.exports = router;
