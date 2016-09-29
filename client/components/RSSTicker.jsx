530500f518304a408bc7015f1a1c12f1b6ea0681
Roni Vegh <Formatted@users.noreply.github.com>
2016-09-29 17:22:20 -0600

    some lint correction (#80)

--- client/components/RSSTicker.jsx
+++ client/components/RSSTicker.jsx
@@ -1,21 +1,18 @@
-import React from 'react';
-import { Component } from 'react';
-import { connect } from 'react-redux';
-import * as actions from '../actions';
-
-
-export class RSSTicker extends Component {
-
-  render () {
+import React, { Component } from 'react';
+// shoud this be a class?Not sure yet, only creating
+// eslint-disable-next-line react/prefer-stateless-function
+class RSSTicker extends Component {
+// eslint-disable-next-line class-methods-use-this
+  render() {
   // const props = this.props;
   // const { store } = this.context;
   // const state = store.getState();
   //   console.log(props, 'store in RSS tocketasda ');
     return (
-    <span>
-    <p>Test me out</p>
-  </span>
-    )
+      <span>
+        <p>Test me out</p>
+      </span>
+    );
   }
 }
 
--- client/components/SettingDisp.jsx
+++ client/components/SettingDisp.jsx
@@ -1,22 +1,25 @@
-import React, { Component } from 'react';
-
+import React, { Component, PropTypes } from 'react';
+// shoud this be a class?Not sure yet, only creating
+// eslint-disable-next-line react/prefer-stateless-function
 class SettingDisp extends Component {
-  constructor(props){
-    super(props);
-  }
+
   render() {
     const { active } = this.props;
     console.log(this.props);
-    console.log(active, "active");
-    if(active) {
-     return (
-          <div>
-            <h1>HEy</h1>
-          </div>
-          );
+    console.log(active, 'active');
+    if (active) {
+      return (
+        <div>
+          <h1>HEy</h1>
+        </div>
+      );
     }
-    return ( <div>empty</div> )
-    }
+    return (<div>empty</div>);
   }
+}
+
+SettingDisp.propTypes = {
+  active: PropTypes.bool,
+};
 
 export default SettingDisp;
