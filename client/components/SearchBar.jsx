2a971e437f44db457b9dc3b820bc4b72853945ed
Jeff Bernstein <jeff-bernstein@users.noreply.github.com>
2016-09-30 15:16:54 -0700

    feat/searchBar: (#94)
    -added searchBar component that opens new window w/ google search

--- client/components/SearchBar.jsx
+++ client/components/SearchBar.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+/* eslint-env browser*/
+import React, { Component } from 'react';
 
-const SearchBar = () => (
-  <div className="searchBar">
-    SearchBar component
-  </div>
-);
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { searchTerm: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    window.open(`https://www.google.com/#q=${this.state.searchTerm}`, '_blank');
+    this.setState({ searchTerm: '' });
+  }
+
+  render() {
+    return (
+      <div className="searchBar">
+        <div>Hello, User</div>
+        <form className="input-group" onSubmit={this.onFormSubmit}>
+          <input
+            className="form-control"
+            placeholder="Search Google!"
+            onChange={this.onInputChange}
+            value={this.state.searchTerm}
+          />
+          <span className="input-group-btn">
+            <button className="btn btn-secondary" type="submit">Submit</button>
+          </span>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
