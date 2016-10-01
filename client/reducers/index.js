0b2b0ee9c90a3e7dd8c269e703084c772d9ce4b3
Jason Holtkamp <holtkam2@gmail.com>
2016-10-01 17:02:54 -0500

    Code cleanup (#118)
    * added a console.log
    
    * styling for settings panel
    
    * add URL click now passes a url and a button name to reducer
    
    * code cleanup

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,13 +2,11 @@ import { combineReducers } from 'redux';
 import toggle from './toggle';
 import rssFeed from './rssFeed';
 import addButtonClick from './addButtonClick';
-import addURLClick from './addURLClick';
 
 const reducers = combineReducers({
   toggle,
   rssFeed,
   addButtonClick,
-  addURLClick,
 });
 
 export default reducers;
