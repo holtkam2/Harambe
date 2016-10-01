e33760eab46fb7e810136de18257079dadf5b664
Patrik Korzinski <patrik.korzinski@gmail.com>
2016-10-01 14:28:11 -0700

    (feat) added state to hold button information

--- client/reducers/addButtonClick.js
+++ client/reducers/addButtonClick.js
@@ -1,11 +1,11 @@
-const addButtonClick = (state = false, action) => {
+const addButtonClick = (state = { buttons: {} }, action) => {
+  console.log('HERE', { ...state });
+  const newState = { ...state };
+  newState.buttons[action.payload] = [];
   switch (action.type) {
     case 'ADD_BUTTON_CLICK':
       console.log('Reducer activated with payload: ', action.payload);
-      return {
-        ...state,
-        buttonName: action.payload,
-      };
+      return newState;
     default:
       return state;
   }
