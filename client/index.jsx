5f4a5273b53821d3d58201168d2cbe8f121271ce
formatted <ronivegh@gmail.com>
2016-10-03 17:18:01 -0600

    async

--- client/index.jsx
+++ client/index.jsx
@@ -8,13 +8,16 @@ import promise from 'redux-promise';
 import createLogger from 'redux-logger';
 import App from './components/App.jsx';
 import reducer from './reducers/index.js';
+import addURL from './actions/index.js';
 
 const logger = createLogger();
 const store = createStore(
   reducer,
   applyMiddleware(thunk, promise, logger),
 );
-
+// $.get('/api/state', data => {
+//   store.dispatch(addURL('testButtonFrom Async', ['http://google.com']));
+// });
 render(
   <Provider store={store}>
     <App />
