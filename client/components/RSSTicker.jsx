dc7eac29b5c944307dab322ec246c4c7dfd95595
Roni Vegh <Formatted@users.noreply.github.com>
2016-10-05 12:36:01 -0600

    Rss n (#158)
    * clean up rss
    
    * rss fiddle mddnesssss

--- client/components/RSSTicker.jsx
+++
@@ -1,9 +0,0 @@
-import React, { PropTypes } from 'react';
-
-const RSSTicker = ({ rssFeed }) => <h2>{ rssFeed }</h2>;
-
-RSSTicker.propTypes = {
-  rssFeed: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
-};
-
-export default RSSTicker;
