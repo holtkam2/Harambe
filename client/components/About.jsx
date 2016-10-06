a116ba8731ae726a431c1096bf65192c71431f27
Jason Holtkamp <holtkam2@gmail.com>
2016-10-06 14:29:13 -0500

    fixed up the about page (#195)

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -91,44 +91,25 @@ export default class DialogExampleCustomWidth extends React.Component {
           <div>
             <h4>about the app</h4>
             <div className="aboutP">
-            No! I want to live! There are still too many things I don't own!
-            Who's brave enough to fly into something we all keep calling a
-            death sphere? I'm a thing. In your time, yes, but nowadays shut
-            up! Besides, these are adult stemcells, harvested from perfectly
-            healthy adults whom I killed for their stemcells.
-            </div>
-            <div className="aboutP">
-            That's the ONLY thing about being a slave. Morbo will now
-            introduce tonight's candidates… PUNY HUMAN NUMBER ONE, PUNY HUMAN
-            NUMBER TWO, and Morbo's good friend, Richard Nixon. Shut up and
-            take my money!
+            Swick is an app that can help you get to the content that you need on the internet much faster. Set up buttons to open several tabs with a single click. Configure RSS feeds to get info streamed to your home page, and recieve notifications when there is relevent breaking news.
             </div>
           </div>
           <div>
-            <h4>what can swick do for you</h4>
+            <h4>how to use Swick</h4>
             <div className="aboutP">
-            Yeah, and if you were the pope they'd be all, "Straighten your
-            pope hat." And "Put on your good vestments." Say it in Russian!
-            Negative, bossy meat creature! And remember, don't do anything
-            that affects anything, unless it turns out you were supposed to,
-            in which case, for the love of God, don't not do it!
+            To configure a new button, open the settings panel and give it a name. Then, enter all the URLs that you want to open as new tabs when the button is clicked.
             </div>
             <div className="aboutP">
-            Bender, we're trying our best. I decline the title of Iron Cook
-             and accept the lesser title of Zinc Saucier, which I just made
-             up. Uhh… also, comes with double prize money. What's with you
-             kids? Every other day it's food, food, food. Alright, I'll get
-             you some stupid food.
+            To configure a new feed to stream at the top of your page, start by giving it a name in the settings panel. Then add RSS feeds url's to it and Swick will automatically compile and process them to display them as one. Click save to make sure it's there the next time you log in.
             </div>
           </div>
           <div>
             <h4>about the dev team</h4>
             <div className="aboutP">
-              Stop it, stop it. It's fine. I will 'destroy' you! Anyhoo, your
-               net-suits will allow you to experience Fry's worm infested
-               bowels as if you were actually wriggling through them. Bender,
-               being God isn't easy.
+              Swick was built by Jeff Bernstein, <a href ="https://www.linkedin.com/in/jasonholtkamp">Jason Holtkamp</a>, <a href ="https://www.linkedin.com/in/pkorzinski?authType=NAME_SEARCH&authToken=VG6I&locale=en_US&trk=tyah&trkInfo=clickedVertical%3Amynetwork%2CclickedEntityId%3A128986459%2CauthType%3ANAME_SEARCH%2Cidx%3A1-1-1%2CtarId%3A1475780559139%2Ctas%3APatrik%20Korzins">Patrik Korzinski</a>, and <a href ="https://www.linkedin.com/in/ronivegh">Roni Vegh</a> as their thesis project for Hack Reactor.
+
             </div>
+            <div> </div>
             <div style={styles.root}>
               <GridList
                 cellHeight={250}
@@ -147,11 +128,9 @@ export default class DialogExampleCustomWidth extends React.Component {
               </GridList>
             </div>
             <div>
+              <h4>technology</h4>
               <div className="aboutP">
-                Stop it, stop it. It's fine. I will 'destroy' you! Anyhoo,
-                your net-suits will allow you to experience Fry's worm
-                infested bowels as if you were actually wriggling through
-                them. Bender, being God isn't easy.
+                Swick was built using ReactJS with Redux for the user interface, NodeJS and Express for the back end, and PostgreSQL for the database. It uses IBM Watson machine learning to analyze and find significant news based on a user's interest list. Have any ideas for new features? Submit a pull request <a href ="https://github.com/HRR18-Harambe/Harambe">here.</a>
               </div>
             </div>
           </div>
