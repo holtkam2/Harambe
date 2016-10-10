c30fa8e077129b6c1ad1a15d2192af87af3ec123
Jason Holtkamp <holtkam2@gmail.com>
2016-10-10 18:16:58 -0500

    now saves user's RSS feed preferences (#265)

--- client/reducers/RSSFeedList.js
+++ client/reducers/RSSFeedList.js
@@ -1,6 +1,8 @@
 const RSSFeedList = (state = ['news', 'sports'], action) => {
   const newState = [...state];
   switch (action.type) {
+    case 'SET_INITIAL_STATE':
+      return action.payload.interests;
 
     case 'ADD_RSS_SELECTION':
       newState.shift();
