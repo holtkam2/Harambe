905f182ebe275363ca106da9011210a1230f2201
Patrik Korzinski <pkorzinski@users.noreply.github.com>
2016-10-11 11:33:54 -0700

    Feature (#275)
    * (feat) boilerplate structure of watsonbar
    
    * (feat) watsonbar functionality complete.
    
    * (feat) watson emotion bar almost there.
    
    * (feat) Watson success.

--- client/components/SentiBar.jsx
+++ client/components/SentiBar.jsx
@@ -2,8 +2,16 @@ import React, { PropTypes } from 'react';
 
 const SentiBar = ({ SentiBarState }) => (
   // SentiBarState has the object with anger fear bla bla bla
-  <div>
-    {SentiBarState.anger}
+  <div className="Sentiment">
+    <div className="emoAnger" style={{ width: `${SentiBarState.anger * 100}%` }} />
+
+    <div className="emoDisgust" style={{ width: `${SentiBarState.disgust * 100}%` }} />
+
+    <div className="emoFear" style={{ width: `${SentiBarState.fear * 100}%` }} />
+
+    <div className="emoJoy" style={{ width: `${SentiBarState.joy * 100}%` }} />
+
+    <div className="emoSadness" style={{ width: `${SentiBarState.sadness * 100}%` }} />
   </div>
 );
 
