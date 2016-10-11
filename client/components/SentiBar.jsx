db6abc5c3cc672bbd2b518d9d37a8824d17a3824
Roni Vegh <Formatted@users.noreply.github.com>
2016-10-11 09:43:57 -0600

    Revert "asd" (#272)

--- client/components/SentiBar.jsx
+++ client/components/SentiBar.jsx
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 
 const SentiBar = ({ SentiBarState }) => (
+  // SentiBarState has the object with anger fear bla bla bla
   <div>
     {SentiBarState.anger}
   </div>
